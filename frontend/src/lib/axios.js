5a6e246148a7ca166d3a6ae0fb8bcb13a8f3bab2
Zain Hassan <155316018+zainhassanee@users.noreply.github.com>
2025-08-09 12:56:25 +0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	baseURL: import.meta.mode === "development" 
 		? "http://localhost:5000/api" 
-		: import.meta.env.VITE_API_URL+"/api",
+		: import.meta.env.VITE_API_URL,
 	withCredentials: true, // send cookies to the server
 });
 
