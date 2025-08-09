06e1d251a93547a6edb105d5c5657b6aaee5ae4d
Zain Hassan <155316018+zainhassanee@users.noreply.github.com>
2025-08-09 13:19:30 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
